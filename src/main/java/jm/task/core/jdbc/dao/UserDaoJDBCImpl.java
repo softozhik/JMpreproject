df59bbd18ef7079518eda502ff654ce13d2a9cec
softozhik <softozhik@live.ru>
2020-10-11 23:04:19 +0300

    обновлен драйвер БД в pom.xml настроено соединение с БД в dao/UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,10 +2,23 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
+        String dburl = "jdbc:mysql://localhost:3306/jmppdb";
+        String dbuser = "jm";
+        String dbpassword = "123456";
+
+            try {
+                Connection dbconnect = DriverManager.getConnection(dburl, dbuser, dbpassword);
+                System.out.println("connect");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
 
     }
 

08a254b7a529c9e085d441e2e505deb1d999679d
softozhik <softozhik@live.ru>
2020-10-18 17:28:12 +0300

    1. подключение к БД 2. переопределение методов в userserviceimpl для вызова через hibernate 3. начал врубаться в создание сессий

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,17 +1,31 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.mapping.MetadataSource;
+import org.hibernate.metamodel.MetadataSources;
+import org.hibernate.service.Service;
+import org.hibernate.service.ServiceRegistry;
 
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.hiberFactory;
+
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
     }
 
 
     @Override
     public void createUsersTable() {
+
+//        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+//        SessionFactory sf = new MetadataSources(registry).addAnnotatedClass(User.class).buildMetadata()
+//                .buildSessionFactory();
+        Session session = hiberFactory().openSession();
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,13 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String dburl = "jdbc:mysql://localhost:3306/jmppdb";
+    private static final String dbuser = "jm";
+    private static final String dbpassword = "123456";
 
     public static Connection getConnection() throws SQLException {
-        final String dburl = "jdbc:mysql://localhost:3306/jmppdb";
-        final String dbuser = "jm";
-        final String dbpassword = "123456";
 
         Connection dbconnect = DriverManager.getConnection(dburl, dbuser, dbpassword);
         //System.out.println("соединение: " + !dbconnect.isClosed());
@@ -22,4 +28,25 @@ public class Util {
         dbconnect.close();
         //System.out.println("отключено: " + dbconnect.isClosed());
     }
+
+    //hibernate
+    public static SessionFactory hiberFactory() {
+        Configuration config = new Configuration()
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.hbm2ddl.auto", "update")
+                .setProperty("hibernate.connection.url", dburl)
+                .setProperty("hibernate.connection.username", dbuser)
+                .setProperty("hibernate.connection.password", dbpassword)
+                .addAnnotatedClass(User.class);
+        StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+        sb.applySettings(config.getProperties());
+        StandardServiceRegistry srr = sb.build();
+        return config.buildSessionFactory(srr);
+    }
+
+    public static void disHiberFactory (SessionFactory hiberFactory) {
+        hiberFactory.close();
+    }
+
     }
